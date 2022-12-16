fbb9b5501a18e0c461463cb4903707af1ae4dae2
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2022-12-16 12:58:56 -0800

    Update

---
+++ C#Dersleri Yucedag/Ders1_FinalProjesi/Ders1_FinalProjesi/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders1_FinalProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " " + comboBox2.Text + " " + maskedTextBox1.Text + " " + maskedTextBox2.Text
+                + dateTimePicker1.Text + " " + maskedTextBox3.Text + " " + maskedTextBox4.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Text = comboBox1.Text;
+            comboBox1.Text = comboBox2.Text;
+            comboBox2.Text = label6.Text;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
