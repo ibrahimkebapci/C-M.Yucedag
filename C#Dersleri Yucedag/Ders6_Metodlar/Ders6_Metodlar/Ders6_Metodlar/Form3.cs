f8bc145670b52f7bb42dcfcdd31620783286bdb5
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2023-01-21 02:50:17 -0800

    Update

---
+++ C#Dersleri Yucedag/Ders6_Metodlar/Ders6_Metodlar/Ders6_Metodlar/Form3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders6_Metodlar
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        public int KupHacim(int s)
+        {
+            int Hacim = s*s*s;
+            return Hacim;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int TekKenar;
+            TekKenar = Convert.ToInt32(textBox1.Text);
+            label1.Text = KupHacim(TekKenar).ToString();
+        }
+    }
+}
