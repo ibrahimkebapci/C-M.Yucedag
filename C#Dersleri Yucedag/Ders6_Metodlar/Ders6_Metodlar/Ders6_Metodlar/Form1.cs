06908ea50a09dc6a04b335a8425fbd3156f260c5
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2023-01-21 02:18:30 -0800

    Update

---
+++ C#Dersleri Yucedag/Ders6_Metodlar/Ders6_Metodlar/Ders6_Metodlar/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders6_Metodlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void AdYaz()
+        {
+            textBox1.Text = "İbrahim";
+            textBox2.Text = "Kebapci";
+            textBox3.Text = "Bilgisayar Mühendisi";
+            textBox4.Text = "22";
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AdYaz();
+        }
+    }
+}
---
+++ C#Dersleri Yucedag/Ders6_Metodlar/Ders6_Metodlar/Ders6_Metodlar/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders6_Metodlar
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public int SayiCarp(int a ,int b ,int c)
+        {
+            int CarpmaSonuc = 0;
+            CarpmaSonuc = a * b * c;
+            return CarpmaSonuc;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = SayiCarp(3, 4, 5).ToString();
+            label2.Text = SayiCarp(4, 5, 6).ToString();
+            label3.Text = SayiCarp(6, 5, 2).ToString();
+        }
+    }
+}
